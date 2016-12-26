34238ce3c6857216574f4446e285b8a2a637503d
刘春江 <liuchunjiang@rd.com>
2016-12-26 18:03:29 +0800

    config

---
+++ core/config.db.php
@@ -0,0 +1,10 @@
+<?php 
+    $db_config["hostname"] = "qdm203823661.my3w.com"; //服务器地址
+    $db_config["username"] = "qdm203823661"; //数据库用户名
+    $db_config["password"] = "hahayxsql"; //数据库密码
+    $db_config["database"] = "qdm203823661_db"; //数据库名称
+    $db_config["charset"] = "utf8";//数据库编码
+    $db_config["pconnect"] = 0;//开启持久连接
+    $db_config["log"] = 1;//开启日志
+    $db_config["logfilepath"] = './';//开启日志
+?>
